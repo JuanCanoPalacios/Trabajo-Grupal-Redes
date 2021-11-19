0664dda5876ebd84ef3e778f8b1e2b35fc6fed67
LucasMedina12 <75556629+LucasMedina12@users.noreply.github.com>
2021-11-18 23:58:00 -0300

    Register mejorado + Validacion

--- e-job/src/components/register/Register.jsx
+++ e-job/src/components/register/Register.jsx
@@ -1,9 +1,61 @@
-import React from "react"
+import React, { useState } from "react"
 import './style_register.css'
+import axios from 'axios'
+import { useForm } from 'react-hook-form'
 
 const Register = () => {
+
+    const {register, formState: { errors }, handleSubmit} = useForm()  /*  Funciones que se utilizan extraidas de react-hook-form  */
                         
-/* className="btn btn-success btn-block btn-lg gradient-custom-4 text-body" */
+
+    const [personas, setPersonas] = useState({     /* State que luego sera rellenado con los valores ingresados y posteado a la BD */
+        nombre_usuario: '',
+        apellido_usuario: '',
+        apodo: '',
+        dni: '',
+        pais: '',
+        localidad: '',
+        telefono: '',
+        email: '',
+        contraseña: '',
+        id_empresa: 1,
+        descripcion: '',
+        imagen: ''
+    })
+
+    const [dni, setDni] = useState(null)    /* State que recibira los dni y los emails existentes para una validacion. */
+
+    let validate = true;
+
+    const onSubmit = (data, e) => {
+
+        axios.get('http://localhost:3001/users/dni&email')
+        .then(res => {
+            const response = res.data.response[0].map(e => e)
+            setDni(response)    /* Se le asigna los dni y emails obtenidos a traves del get */ 
+            console.log(dni)
+        })
+
+
+        dni.forEach(e => {
+            if (e.dni === data.dni || e.email === data.email) validate = false      /* Valida que el mail y dni ingresados por el usuario no existan ya en la BD */
+        })
+
+        validate === true ? setPersonas(data) : alert("El DNI o Email ingresado ya esta existente dentro de nuestra base de datos.")  /* Muestra alerta en caso de que si existan en la BD */
+
+        
+        if (validate){
+            axios.post('http://localhost:3001/users',{
+                ...personas,
+                descripcion: 'asd',
+                imagen: 'asd',
+                id_empresa:1
+            })
+            console.log(personas)
+        }
+
+        e.target.reset()
+    } 
 
     return(
     <section className="vh-100 bg-image bk_register" >
@@ -15,55 +67,82 @@ const Register = () => {
                     <div className="card-body p-5">
                     <h2 className="text-uppercase text-center mb-5">Crear una cuenta</h2>
 
-                    <form>
+                    <form  onSubmit={handleSubmit(onSubmit)}>
                         <div className="form-outline mb-4">
-                        <input type="text" id="form3Example1cg" className="form-control form-control-lg" />
-                        <label className="form-label" for="form3Example1cg">Nombre</label>
+                        <input type="text"  className="form-control form-control-lg" {...register('nombre_usuario', { required: { value: true, message: 'Nombre Requerido' } })}/>
+                        <span className="text-danger text-small d-block mb-2">
+                            { errors?.nombre_usuario?.message }
+                        </span>
+                        <label className="form-label" for="nombre">Nombre</label>
                         </div>
 
                         <div className="form-outline mb-4">
-                        <input type="text" id="form3Example1cg" className="form-control form-control-lg" />
-                        <label className="form-label" for="form3Example1cg">Apellido</label>
+                        <input type="text"  className="form-control form-control-lg" {...register('apellido_usuario', { required: { value: true, message: 'Apellido Requerido' } })}/>
+                        <span className="text-danger text-small d-block mb-2">
+                            { errors?.apellido_usuario?.message }
+                        </span>
+                        <label className="form-label" for="apellido">Apellido</label>
                         </div>
 
                         <div className="form-outline mb-4">
-                        <input type="text" id="form3Example1cg" className="form-control form-control-lg" />
-                        <label className="form-label" for="form3Example1cg">Apodo</label>
+                        <input type="text" className="form-control form-control-lg"  {...register('apodo', { required: { value: true, message: 'Apodo Requerido' } })}/>
+                        <span className="text-danger text-small d-block mb-2">
+                            { errors?.apodo?.message }
+                        </span>
+                        <label className="form-label" for="apodo">Apodo</label>
                         </div>
                         
                         
                         <div className="form-outline mb-4">
-                        <input type="text" id="form3Example1cg" className="form-control form-control-lg" />
-                        <label className="form-label" for="form3Example1cg">Dni</label>
+                        <input type="number" className="form-control form-control-lg"  {...register('dni', { required: { value: true, message: 'Dni Requerido' }, valueAsNumber: true })}/>
+                        <span className="text-danger text-small d-block mb-2">
+                            { errors?.dni?.message }
+                        </span>
+                        <label className="form-label" for="dni">Dni</label>
                         </div>
 
                         <div className="form-outline mb-4">
-                        <input type="text" id="form3Example1cg" className="form-control form-control-lg" />
-                        <label className="form-label" for="form3Example1cg">Pais</label>
+                        <input type="text"  className="form-control form-control-lg" {...register('pais', { required: { value: true, message: 'Pais Requerido' } })} />
+                        <span className="text-danger text-small d-block mb-2">
+                            { errors?.pais?.message }
+                        </span>
+                        <label className="form-label" for="pais">Pais</label>
                         </div>
 
                         <div className="form-outline mb-4">
-                        <input type="text" id="form3Example1cg" className="form-control form-control-lg" />
-                        <label className="form-label" for="form3Example1cg">Localidad</label>
+                        <input type="text"  className="form-control form-control-lg" {...register('localidad', { required: { value: true, message: 'Localidad Requerido' } })}/>
+                        <span className="text-danger text-small d-block mb-2">
+                            { errors?.localidad?.message }
+                        </span>
+                        <label className="form-label" for="localidad">Localidad</label>
                         </div>
 
                         <div className="form-outline mb-4">
-                        <input type="text" id="form3Example1cg" className="form-control form-control-lg" />
-                        <label className="form-label" for="form3Example1cg">Telefono</label>
+                        <input type="number"  className="form-control form-control-lg" {...register('telefono', { required: { value: true, message: 'Telefono Requerido' } })}/>
+                        <span className="text-danger text-small d-block mb-2">
+                            { errors?.telefono?.message }
+                        </span>
+                        <label className="form-label" for="telefono">Telefono</label>
                         </div>
 
                         <div className="form-outline mb-4">
-                        <input type="email" id="form3Example3cg" className="form-control form-control-lg" />
-                        <label className="form-label" for="form3Example3cg">Email</label>
+                        <input type="email"  className="form-control form-control-lg" {...register('email', { required: { value: true, message: 'Email Requerido' } })}/>
+                        <span className="text-danger text-small d-block mb-2">
+                            { errors?.email?.message }
+                        </span>
+                        <label className="form-label" for="email">Email</label>
                         </div>
 
                         <div className="form-outline mb-4">
-                        <input type="password" id="form3Example4cg" className="form-control form-control-lg" />
-                        <label className="form-label" for="form3Example4cg">Contraseña</label>
+                        <input type="password"  className="form-control form-control-lg" {...register('contraseña', { required: { value: true, message: 'Contraseña Requerida' } })}  />
+                        <span className="text-danger text-small d-block mb-2">
+                            { errors?.contraseña?.message }
+                        </span>
+                        <label className="form-label" for="contraseña">Contraseña</label>
                         </div>
 
                         <div className="d-flex justify-content-center">
-                        <button type="button" className='css-button-sliding-to-top--blue ' >Registrarse</button>
+                        <button type="submit" className='css-button-sliding-to-top--blue ' >Registrarse</button>
                         </div>
                     </form>
 
